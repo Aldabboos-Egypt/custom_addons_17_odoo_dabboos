86445f8fcc8b3c0d4eb4c56466b3b2f95b1bd9fc
mahmoud galy <mahmoudgaly2019@gmail.com>
2025-01-25 14:00:10 +0200

    update

--- sh_sales_person_customer/models/res_partner.py
+++ sh_sales_person_customer/models/res_partner.py
@@ -9,7 +9,7 @@ class ResPartner(models.Model):
 
     sales_persons_ids = fields.Many2many(
         "res.users",
-        string="Allocate Sales Persons"
+        string="Allocate Sales Persons",domain="[ ('company_id', '=',company_id) ]",
     )
 
     def action_sales_person_customer_update(self):
@@ -34,54 +34,54 @@ class ResPartner(models.Model):
             'ir.actions.act_window'
         }
 
-    # To apply domain to customer search
-
-    @api.model
-    def name_search(self, name, args=None, operator="ilike", limit=100):
-        search_domain = [("name", operator, name)]
-
-        if(
-            self.env.user.has_group("sales_team.group_sale_salesman") and not
-            (self.env.user.has_group("sales_team.group_sale_salesman_all_leads"))
-        ):
-            search_domain = ["|", "|","|",
-                             ("sales_persons_ids", "in", self.env.user.id),
-                             ("user_id", "=", self.env.user.id),
-                             ("id", "=", self.env.user.partner_id.id),
-                            #  ("employee_ids","!=",False),
-                             ("name", operator, name)
-                             ]
-        partners = self.search(search_domain)
-        return partners.name_get()
-
-    # To apply domain to menu action
-    @api.model
-    def search(self, args, offset=0, limit=None, order=None, count=False):
-        _ = self._context or {}
-        if(
-            self.env.user.has_group("sales_team.group_sale_salesman") and not
-            (self.env.user.has_group("sales_team.group_sale_salesman_all_leads"))
-        ):
-            # args += ["|", "|","|",
-            #          ("sales_persons_ids", "in", self.env.user.id),
-            #          ("user_id", "=", self.env.user.id),
-            #          ("id", "=", self.env.user.partner_id.id),
-            #          ("employee_ids","!=",False)
-            #          ]
-
-            args += ["|", "|",
-                     ("sales_persons_ids", "in", self.env.user.id),
-                     ("user_id", "=", self.env.user.id),
-                     ("id", "=", self.env.user.partner_id.id),
-                    #  ("employee_ids","!=",False)
-                     ]
-        return super(ResPartner, self).search(
-            args,
-            offset=offset,
-            limit=limit,
-            order=order,
-            # count=count,
-        )
+    # # To apply domain to customer search
+    #
+    # @api.model
+    # def name_search(self, name, args=None, operator="ilike", limit=100):
+    #     search_domain = [("name", operator, name)]
+    #
+    #     if(
+    #         self.env.user.has_group("sales_team.group_sale_salesman") and not
+    #         (self.env.user.has_group("sales_team.group_sale_salesman_all_leads"))
+    #     ):
+    #         search_domain = ["|", "|","|",
+    #                          ("sales_persons_ids", "in", self.env.user.id),
+    #                          ("user_id", "=", self.env.user.id),
+    #                          ("id", "=", self.env.user.partner_id.id),
+    #                         #  ("employee_ids","!=",False),
+    #                          ("name", operator, name)
+    #                          ]
+    #     partners = self.search(search_domain)
+    #     return partners.name_get()
+    #
+    # # To apply domain to menu action
+    # @api.model
+    # def search(self, args, offset=0, limit=None, order=None, count=False):
+    #     _ = self._context or {}
+    #     if(
+    #         self.env.user.has_group("sales_team.group_sale_salesman") and not
+    #         (self.env.user.has_group("sales_team.group_sale_salesman_all_leads"))
+    #     ):
+    #         # args += ["|", "|","|",
+    #         #          ("sales_persons_ids", "in", self.env.user.id),
+    #         #          ("user_id", "=", self.env.user.id),
+    #         #          ("id", "=", self.env.user.partner_id.id),
+    #         #          ("employee_ids","!=",False)
+    #         #          ]
+    #
+    #         args += ["|", "|",
+    #                  ("sales_persons_ids", "in", self.env.user.id),
+    #                  ("user_id", "=", self.env.user.id),
+    #                  ("id", "=", self.env.user.partner_id.id),
+    #                 #  ("employee_ids","!=",False)
+    #                  ]
+    #     return super(ResPartner, self).search(
+    #         args,
+    #         offset=offset,
+    #         limit=limit,
+    #         order=order,
+    #         # count=count,
+    #     )
 
     @api.model
     def default_get(self, fields):
